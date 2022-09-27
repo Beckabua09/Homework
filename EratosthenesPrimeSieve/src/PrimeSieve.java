94ce28664c1342db1d6bce4949a5bfdfc8a94fe7
Noah <noah.oberndorfer123@gmail.com>
2022-09-27 09:15:01 +0200

    Fertige Hausübung

--- EratosthenesPrimeSieve/src/PrimeSieve.java
+++ EratosthenesPrimeSieve/src/PrimeSieve.java
@@ -1,44 +1,47 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class PrimeSieve {
-    static Scanner sc = new Scanner(System.in);
+
+    public static List<Integer> numbers = new ArrayList<>();
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Bitte geben Sie das Maximum an.");
         int max = sc.nextInt();
-        for (int i = 0; i <= max; i++){
-            if (prime.isPrime(max) == false){
-                System.out.println(i);
-            }
-        }
+        prime.isPrime(max);
+        prime.printPrimes();
     }
     static EratosthenesPrimeSieve prime = new EratosthenesPrimeSieve() {
         @Override
         public boolean isPrime(int p) {
             boolean[] gestrichen = new boolean[p];
-            for (int i = 2; i <= gestrichen.length; i++){
-                gestrichen[i] = false;
-            }
-            for (int i = 2; i <= Math.sqrt(p); i++){
-                if(gestrichen[i] == false){
-                    System.out.println(i);
-                    System.out.println(", ");
-                    for (int j = i*i; j <= p; j += i){
+
+
+            for (int i = 2; i <= Math.sqrt(p); i++) {
+                if (gestrichen[i] == false) {
+                    for (int j = i * i; j <= (p - 1); j += i) {
                         gestrichen[j] = true;
                     }
                 }
             }
-            for (int i = Math.sqrt(p)+1; i <= p; i++){
-                if (gestrichen[i] == false){
-                    System.out.println(i);
-                    System.out.println(", ");
+            for (int i = 2; i < p; i++) {
+                if (gestrichen[i] == false) {
+                    numbers.add(i);
                 }
             }
-            return true;
+
+
+            return false;
         }
+
+
 
         @Override
         public void printPrimes(){
-            //System.out.println(int x);
+            for (int i = 0; i < numbers.size(); i++)
+            {
+                System.out.print(numbers.get(i) + ", ");
+            }
         }
     };
 }
